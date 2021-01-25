9ab4cfd4d53b2f903be3a60f1ab2aa0e5ad46081
ichristine180 <ichristine180@gmail.com>
2021-01-25 12:03:36 +0200

    error

--- src/database/queries/Points.js
+++ src/database/queries/Points.js
@@ -147,20 +147,20 @@ catone, cattwo, exam, teacherid, term
    = $1 and points.subjectname = $2 and points.term = $3 and points.year = $4 and position(LOWER($5) in LOWER(studentnames) || LOWER(regestrationNumber))>0`;
 
 //Term 1 data for year report
-export const catOneSumPerTerm1 = `SELECT sum(catone) as catoneSumInTerm1 FROM points where studentid=$1 and levelid=$2 and term='1' and year=$3`;
-export const catTwoSumPerTerm1 = `SELECT sum(cattwo) as cattwoSumInTerm1 FROM points where studentid=$1 and levelid=$2 and term='1' and year=$3`;
-export const examSumPerTerm1 = `SELECT sum(exam)  as examSumPerTerm1 FROM points where studentid=$1 and levelid=$2 and term='1' and year=$3`;
+export const catOneSumPerTerm1 = `SELECT sum(catone) FROM points where studentid=$1 and levelid=$2 and term='1' and year=$3`;
+export const catTwoSumPerTerm1 = `SELECT sum(cattwo)  FROM points where studentid=$1 and levelid=$2 and term='1' and year=$3`;
+export const examSumPerTerm1 = `SELECT sum(exam) FROM points where studentid=$1 and levelid=$2 and term='1' and year=$3`;
 
 
 //Term 2 data for year report
-export const catOneSumPerTerm2 = `SELECT sum(catone) as catoneSumInTerm2 FROM points where studentid=$1 and levelid=$2 and term='2' and year=$3`;
-export const catTwoSumPerTerm2 = `SELECT sum(cattwo) as cattwoSumInTerm2 FROM points where studentid=$1 and levelid=$2 and term='2' and year=$3`;
-export const examSumPerTerm2 = `SELECT sum(exam)  as examSumPerTerm2 FROM points where studentid=$1 and levelid=$2 and term='2' and year=$3`;
+export const catOneSumPerTerm2 = `SELECT sum(catone)  FROM points where studentid=$1 and levelid=$2 and term='2' and year=$3`;
+export const catTwoSumPerTerm2 = `SELECT sum(cattwo) FROM points where studentid=$1 and levelid=$2 and term='2' and year=$3`;
+export const examSumPerTerm2 = `SELECT sum(exam) FROM points where studentid=$1 and levelid=$2 and term='2' and year=$3`;
 
 //Term 3 data for year report
-export const catOneSumPerTerm3 = `SELECT sum(catone) as catoneSumInTerm3 FROM points where studentid=$1 and levelid=$2 and term='3' and year=$3`;
-export const catTwoSumPerTerm3 = `SELECT sum(cattwo) as cattwoSumInTerm3 FROM points where studentid=$1 and levelid=$2 and term='3' and year=$3`;
-export const examSumPerTerm3 = `SELECT sum(exam)  as examSumPerTerm3 FROM points where studentid=$1 and levelid=$2 and term='3' and year=$3`;
+export const catOneSumPerTerm3 = `SELECT sum(catone) FROM points where studentid=$1 and levelid=$2 and term='3' and year=$3`;
+export const catTwoSumPerTerm3 = `SELECT sum(cattwo) FROM points where studentid=$1 and levelid=$2 and term='3' and year=$3`;
+export const examSumPerTerm3 = `SELECT sum(exam) FROM points where studentid=$1 and levelid=$2 and term='3' and year=$3`;
 
 //Positions for each term
 export const getPositionsByClassInTerm1 = `SELECT * from positions inner join student_class on positions.studentid=student_class.studentid inner join  class on class.classid=student_class.classid WHERE term='1' and class.classid=$1`;
